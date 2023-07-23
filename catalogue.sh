d70da98370557f1beb3330fbf7318970cf027d6b
sreedhar <sreedhar70755@gmail.com>
2023-07-23 18:59:44 +0530

    code

--- catalogue.sh
+++ catalogue.sh
@@ -2,6 +2,7 @@ cp catalogue.service /etc/systemd/system/catalogue.service
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
 useradd roboshop
 mkdir /app
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
