99fb307ecdb21be6a8a4cdfe8009c1e968d3b0b1
sreedhar <sreedhar70755@gmail.com>
2023-07-23 16:21:46 +0530

    code

---
+++ shipping.sh
@@ -0,0 +1,20 @@
+cp shippinf.service /etc/systemd/system/shipping.service
+
+
+yum install maven -y
+useradd roboshop
+mkdir /app
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
+cd /app
+unzip /tmp/shipping.zip
+cd /app
+mvn clean package
+mv target/shipping-1.0.jar shipping.jar
+
+systemctl daemon-reload
+systemctl enable shipping
+systemctl start shipping
+yum install mysql -y
+mysql -h mysql.cloudev7.online -uroot -pRoboShop@1 < /app/schema/shipping.sql
+systemctl restart shipping
+
