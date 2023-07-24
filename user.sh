b2faba5968127337d465b3ae14101897d5d48384
sreedhar <sreedhar70755@gmail.com>
2023-07-24 16:00:51 +0530

    code

---
+++ user.sh
@@ -0,0 +1,27 @@
+cp user.service /etc/systemd/system/user.service
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+id roboshop
+if [$? -ne 0]
+then
+useradd roboshop
+fi
+rm -rf /app
+mkdir /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+cd /app
+unzip /tmp/user.zip
+cd /app
+npm install
+
+yum install mongodb-org-shell -y
+
+mongo --host mongodb.cloudev7.online </app/schema/user.js
+
+systemctl daemon-reload
+systemctl enable user
+systemctl start user
