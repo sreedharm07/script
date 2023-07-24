2856d859ff1378e686a40de9c8a6ad57091efd2b
sreedhar <sreedhar70755@gmail.com>
2023-07-24 12:01:14 +0530

    code

--- user.sh
+++ user.sh
@@ -5,6 +5,7 @@ curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 
 yum install nodejs -y
 useradd roboshop
+rm -rf /app
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
 cd /app
