025426522426489396420c9fb40c2300d024c2ab
sreedhar <sreedhar70755@gmail.com>
2023-07-26 12:21:13 +0530

    code

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -2,9 +2,9 @@ curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
 
 yum install rabbitmq-server -y
-systemctl start rabbitmq-server
 
 systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+#rabbitmqctl add_user roboshop roboshop123
+#rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
