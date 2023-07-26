bc00d6790ba45743f8eb0fa0074dafd4efc238a3
sreedhar <sreedhar70755@gmail.com>
2023-07-26 12:59:59 +0530

    code

--- frontend.sh
+++ frontend.sh
@@ -1,18 +1,13 @@
-echo -e "\e[31m>>>>>> copying the config file <<<<<<<<<<\e[0m"
-cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>>/tmp/roboshop.log
+cp nginx.conf /etc/nginx/default.d/roboshop.conf
 
-echo -e "\e[31m >>>>>> installing nginx <<<<<<<<<<\e[0m"
-yum install nginx -y  &>>/tmp/roboshop.log
+yum install nginx -y
 
-echo -e "\e[31m >>>>>> downloading <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log
-cd /usr/share/nginx/html  &>>/tmp/roboshop.log
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
-echo -e "\e[31m >>>>>> unziping <<<<<<<<<<\e[0m"
-unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip
 
-echo -e "\e[31m >>>>>> restarting <<<<<<<<<<\e[0m"
-systemctl enable nginx  &>>/tmp/roboshop.log
-systemctl start nginx   &>>/tmp/roboshop.log
+systemctl enable nginx
+systemctl start nginx
 systemctl restart nginx
