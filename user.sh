eb5313a9818eaf8f14d662965c00c207dafc67cf
sreedhar <sreedhar70755@gmail.com>
2023-07-26 14:58:11 +0530

    code

--- user.sh
+++ user.sh
@@ -1,33 +1,33 @@
 echo -e "\e [32m >>>>>>> copying repos<<<<<<\e[0m"
-cp user.service /etc/systemd/system/user.service
-cp mongo.repo /etc/yum.repos.d/mongo.repo
+cp user.service /etc/systemd/system/user.service  &>> /tmp/roboshop.log
+cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>setting up repos<<<<<< \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>> /tmp/roboshop.log
+yum install nodejs -y  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>nodejs<<<<<< \e[0m"
-useradd roboshop
+useradd roboshop  &>> /tmp/roboshop.log
 
-rm -rf /app
-mkdir /app
+rm -rf /app &>> /tmp/roboshop.log
+mkdir /app  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>downloading app <<<<<< \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
-unzip /tmp/user.zip
-cd /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>> /tmp/roboshop.log
+cd /app  &>> /tmp/roboshop.log
+unzip /tmp/user.zip  &>> /tmp/roboshop.log
+cd /app  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>installing dependencies<<<<<< \e[0m"
-npm install
+npm install  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>installing mongodb<<<<<< \e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>mongodb schema<<<<<< \e[0m"
-mongo --host mongodb.cloudev7.online </app/schema/user.js
+mongo --host mongodb.cloudev7.online </app/schema/user.js  &>> /tmp/roboshop.log
 
 echo -e "\e [32m >>>>>>>systemct restart<<<<<< \e[0m"
-systemctl daemon-reload
-systemctl enable user
+systemctl daemon-reload  &>> /tmp/roboshop.log
+systemctl enable user  &>> /tmp/roboshop.log
 systemctl start user
