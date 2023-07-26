ca848f525f60f7650f8b533404cbc50dfc2f9b0c
sreedhar <sreedhar70755@gmail.com>
2023-07-26 16:15:16 +0530

    code

--- common.sh
+++ common.sh
@@ -1,40 +1,42 @@
+log=/tmp/roboshop.log
+
 function_nodejs() {
 
 echo -e "\e[32m>>>>>>copying the service an repos <<<<<<<<\e[0m"
 
-cp ${component}.service /etc/systemd/system/${component}.service   &>>/tmp/roboshop.log
+cp ${component}.service /etc/systemd/system/${component}.service   &>>$log
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 
 echo -e "\e[32m>>>>>>>>>nodejs settingup <<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>/tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>>$log
 
 echo -e "\e[32m>>>>>>installing nodejs <<<<<<<<\e[0m"
-yum install nodejs -y  &>>/tmp/roboshop.log
+yum install nodejs -y  &>>$log
 
 echo -e "\e[32m>>>>>>creating user and directory<<<<<<<<\e[0m"
-useradd roboshop  &>>/tmp/roboshop.log
-rm -rf /app  &>>/tmp/roboshop.log
-mkdir /app  &>>/tmp/roboshop.log
+useradd roboshop  &>>$log
+rm -rf /app  &>>$log
+mkdir /app  &>>$log
 
 echo -e "\e[32m>>>>>>downloading the app<<<<<<<<\e[0m"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>/tmp/roboshop.log
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>$log
 cd /app
 
 echo -e "\e[32m>>>>>>unzipping<<<<<<<<\e[0m"
-unzip /tmp/${component}.zip  &>>/tmp/roboshop.log
+unzip /tmp/${component}.zip  &>>$log
 cd /app
-npm install  &>>/tmp/roboshop.log
+npm install  &>>$log
 
 systemctl enable ${component}
 
 echo -e "\e[32m>>>>>>installing mongo<<<<<<<<\e[0m"
-yum install mongodb-org-shell -y  &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y  &>>$log
 
 echo -e "\e[32m>>>>>>setting schema<<<<<<<<\e[0m"
-mongo --host mongodb.cloudev7.online </app/schema/${component}.js  &>>/tmp/roboshop.log
+mongo --host mongodb.cloudev7.online </app/schema/${component}.js  &>>$log
 
 echo -e "\e[32m>>>>>>crestarting<<<<<<<<\e[0m"
-systemctl daemon-reload  &>>/tmp/roboshop.log
-systemctl enable ${component}  &>>/tmp/roboshop.log
-systemctl start ${component}  &>>/tmp/roboshop.log
+systemctl daemon-reload  &>>$log
+systemctl enable ${component}  &>>$log
+systemctl start ${component}  &>>$log
 }
--- user.sh
+++ user.sh
@@ -1,33 +1,4 @@
-echo -e "\e[33m>>>>>>> copying repos<<<<<<\e[0m"
-cp user.service /etc/systemd/system/user.service  &>> /tmp/roboshop.log
-cp mongo.repo /etc/yum.repos.d/mongo.repo  &>> /tmp/roboshop.log
+component=user
+source common.sh
 
-echo -e "\e[33m >>>>>>> setting up repos <<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash   &>> /tmp/roboshop.log
-yum install nodejs -y  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>nodejs<<<<<<\e[0m"
-useradd roboshop  &>> /tmp/roboshop.log
-
-rm -rf /app &>> /tmp/roboshop.log
-mkdir /app  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>downloading app <<<<<< \e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip  &>> /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
-unzip /tmp/user.zip  &>> /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>installing dependencies<<<<<< \e[0m"
-npm install  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>installing mongodb<<<<<< \e[0m"
-yum install mongodb-org-shell -y  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>mongodb schema<<<<<< \e[0m"
-mongo --host mongodb.cloudev7.online </app/schema/user.js  &>> /tmp/roboshop.log
-
-echo -e "\e[33m>>>>>>>systemct restart<<<<<< \e[0m"
-systemctl daemon-reload  &>> /tmp/roboshop.log
-systemctl enable user  &>> /tmp/roboshop.log
-systemctl start user
+function_nodejs
