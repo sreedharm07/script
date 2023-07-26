8dfb36f23e766284cebd1f9e2de6954ea957b880
sreedhar <sreedhar70755@gmail.com>
2023-07-26 15:50:52 +0530

    code

--- cart.sh
+++ cart.sh
@@ -1,29 +1,31 @@
+log = /tmp/roboshop.log
+
 echo -e "\e[34m >>>>>>coping service file <<<<<<<\e[0m"
-cp cart.service /etc/systemd/system/cart.service  &>> /tmp/roboshop.log
+cp cart.service /etc/systemd/system/cart.service  &>> $log
 
 echo -e "\e[34m >>>>>>settingup repos<<<<<<<\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> /tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash  &>> $log
 
 echo -e "\e[34m >>>>>>installing nodejs<<<<<<<\e[0m"
-yum install nodejs -y   &>> /tmp/roboshop.log
+yum install nodejs -y   &>> $log
 
 echo -e "\e[34m >>>>>>creating repo<<<<<<<\e[0m"
-useradd roboshop  &>> /tmp/roboshop.log
-rm -rf /app  &>> /tmp/roboshop.log
-mkdir /app  &>> /tmp/roboshop.log
+useradd roboshop  &>> $log
+rm -rf /app  &>> $log
+mkdir /app  &>> $log
 
 echo -e "\e[34m >>>>>>downloading app<<<<<<<\e[0m"
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>> /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip  &>> $log
+cd /app  &>> $log
 
 echo -e "\e[34m >>>>>>unzip<<<<<<<\e[0m"
-unzip /tmp/cart.zip  &>> /tmp/roboshop.log
-cd /app  &>> /tmp/roboshop.log
+unzip /tmp/cart.zip  &>> $log
+cd /app  &>> $log
 
 echo -e "\e[34m >>>>>>installing dependencies<<<<<<<\e[0m"
-npm install  &>> /tmp/roboshop.log
+npm install  &>> $log
 
 echo -e "\e[34m >>>>>>systemctl starts<<<<<<<\e[0m"
-systemctl daemon-reload  &>> /tmp/roboshop.log
-systemctl enable cart  &>> /tmp/roboshop.log
-systemctl start cart  &>> /tmp/roboshop.log
+systemctl daemon-reload  &>> $log
+systemctl enable cart  &>> $log
+systemctl start cart  &>> $log
