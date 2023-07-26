bc22cf457c265bd2c61c3f0daa637e673c00edc9
sreedhar <sreedhar70755@gmail.com>
2023-07-26 11:05:59 +0530

    code

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@ yum install mongodb-org -y
 systemctl enable mongod
 systemctl start mongod
 
-sed -i 's /127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i 's \127.0.0.1\0.0.0.0\' /etc/mongod.conf
 #sed -i 's /127.0.0.1/0.0.0.0/' /etc/mongod/mongo.conf
 
 systemctl restart mongod
