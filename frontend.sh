dbd0dd550127a833cb28699c758f6e3614f8628f
sreedhar <sreedhar70755@gmail.com>
2023-07-26 14:18:26 +0530

    code

--- frontend.sh
+++ frontend.sh
@@ -1,13 +1,18 @@
-cp nginx.conf /etc/nginx/default.d/roboshop.conf
+echo -e "\e[31m>>>>>> copying the config file <<<<<<<<<<\e[0m"
+cp nginx.conf /etc/nginx/default.d/roboshop.conf  &>>/tmp/roboshop.log
 
-yum install nginx -y
+echo -e "\e[31m >>>>>> installing nginx <<<<<<<<<<\e[0m"
+yum install nginx -y  &>>/tmp/roboshop.log
 
+echo -e "\e[31m >>>>>> downloading <<<<<<<<<<\e[0m"
 rm -rf /usr/share/nginx/html/*
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>>/tmp/roboshop.log   &>>/tmp/roboshop.log
+cd /usr/share/nginx/html  &>>/tmp/roboshop.log
 
-cd /usr/share/nginx/html
-unzip /tmp/frontend.zip
+echo -e "\e[31m >>>>>> unziping <<<<<<<<<<\e[0m"
+unzip /tmp/frontend.zip  &>>/tmp/roboshop.log
 
-systemctl enable nginx
-systemctl start nginx
+echo -e "\e[31m >>>>>> restarting <<<<<<<<<<\e[0m"
+systemctl enable nginx  &>>/tmp/roboshop.log
+systemctl start nginx   &>>/tmp/roboshop.log
 systemctl restart nginx
