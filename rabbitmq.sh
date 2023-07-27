4b400ec21f1d408550b9116b1e0478c3becc70f9
sreedhar <sreedhar70755@gmail.com>
2023-07-27 11:42:36 +0530

    code

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,22 +1,23 @@
-log = &>>/tmp/roboshop.log
+source common.sh
+log = /tmp/roboshop.log
 
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  $log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash  &>>log
 function_status
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  $log
-function_status
-
-yum install rabbitmq-server -y  $log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>>log
 function_status
 
-systemctl enable rabbitmq-server  $log
-function_status
-systemctl start rabbitmq-server  $log
+yum install rabbitmq-server -y  &>>log
 function_status
 
- id roboshop  $log
+systemctl enable rabbitmq-server  &>>log
+function_status
+systemctl start rabbitmq-server  &>>log
+function_status
+
+ id roboshop  &>>log
 if [$? -ne 0 ] ; then
-rabbitmqctl add_user roboshop roboshop123  $log
+rabbitmqctl add_user roboshop roboshop123  &>>log
 fi
 function_status
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  $log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"  &>>log
 function_status
